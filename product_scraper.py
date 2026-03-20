463a7cc616632a9cf97891cc3a982005e8feef20
Yuvaan Kaarthikeyaa <72755692+karthik008-cod@users.noreply.github.com>
2026-03-20 22:44:51 +0530

    Comment out headless option in Chrome setup

--- product_scraper.py
+++ product_scraper.py
@@ -11,7 +11,7 @@ search_query = sys.argv[1]
 # Set up Chrome to bypass Bot Detection
 options = Options()
 options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36")
-options.add_argument("--headless=new") # Run in background
+#options.add_argument("--headless=new") # Run in background
 options.add_argument("--disable-gpu")
 options.add_argument("--window-size=1920,1080")
 options.add_argument("--disable-blink-features=AutomationControlled")
